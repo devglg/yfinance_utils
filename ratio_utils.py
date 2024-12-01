a4b50fa58d7b21300c581718c46d489c349386b1
L Gracia Tech <124262275+devglg@users.noreply.github.com>
2024-12-01 15:18:18 -0600

    Add files via upload

---
+++ ratio_utils.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Lehi Gracia
+#
+
+# https://corporatefinanceinstitute.com/resources/accounting/financial-ratios/
+
+import utils.financials_utils as fu
+
+###
+### GENERAL
+###
+
+
+## TODO: COR should be changed to COGS
+def get_net_revenue(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    return ist["TotalRevenue"] - ist["CostOfRevenue"]
+
+def get_net_income(tick, period = 0):
+    return get_net_revenue(tick, period)
+
+
+def get_net_sales(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    COGS = ist["CostOfRevenue"] \
+            - ist["SellingGeneralAndAdministration"] \
+            - ist["SellingAndMarketingExpense"] \
+            - ist["GeneralAndAdministrativeExpense"] \
+            - ist["OtherGandA"] \
+            - ist['ResearchAndDevelopment']
+    return ist["TotalRevenue"] - COGS
+
+
+def get_avg_assets(tick, period = 0):
+    bs = fu.get_bs(tick, period = period)
+    bs1 = fu.get_bs(tick, period = period + 1)
+    avg_assets = (bs["TotalAssets"] + bs1["TotalAssets"]) / 2
+    return avg_assets
+
+def get_avg_inventory(tick, period = 0):
+    bs = fu.get_bs(tick, period=period)
+    bs1 = fu.get_bs(tick, period=period + 1)
+    avg_inventory = (bs["Inventory"] + bs1["Inventory"]) / 2
+    return avg_inventory
+
+
+###
+### LIQUIDITY RATIOS: ability to repay both short- and long-term obligations
+###
+
+### company’s ability to pay off short-term liabilities with current assets###
+def get_current_ratio(tick):
+    return tick.info["currentRatio"]
+
+### company’s ability to pay off short-term liabilities with quick assets###
+def get_acid_test_ratio(tick, period = 0):
+    bs = fu.get_bs(tick, period=period)
+    acid = bs["CurrentAssets"] - bs["Inventory"]
+    return acid / bs["CurrentLiabilities"]
+    
+### company’s ability to pay off short-term liabilities with cash and cash equivalents###
+def get_cash_ratio(tick, period = 0):
+    bs = fu.get_bs(tick, period = period)
+    return bs["CashAndCashEquivalents"] / bs["CurrentLiabilities"]
+
+### number of times a company can pay off current liabilities with the cash generated in a given period###
+def get_operating_cash_flow_ratio(tick, period = 0):
+    bs = fu.get_bs(tick, period = period)
+    cf = fu.get_cashflow(tick, period = period)
+    return cf["OperatingCashFlow"] / bs["CurrentLiabilities"]
+
+
+###
+### LEVERAGE RATIOS: amount of capital that comes from debt
+###
+
+### assets that are provided from debt###
+def get_debt_ratio(tick, period = 0):
+    bs = fu.get_bs(tick, period = period)
+    return bs["TotalLiabilitiesNetMinorityInterest"] / bs["TotalAssets"]
+
+### weight of total debt and financial liabilities against shareholders’ equity###
+def get_debt_to_equity_ratio(tick, period = 0):
+    bs = fu.get_bs(tick, period = period)
+    return bs["TotalLiabilitiesNetMinorityInterest"] / bs["StockholdersEquity"]
+
+### company can pay its interest expenses###
+def get_interest_coverage_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    return ist["OperatingIncome"] / ist["InterestExpense"]
+
+### company can pay its debt obligations###
+def get_service_coverage_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    bs = fu.get_bs(tick, period = period)
+    return ist["OperatingIncome"] / bs["TotalDebt"]
+
+
+###
+### EFFICIENCY RATIOS: how well a company is utilizing its assets and resources
+###
+
+### ability to generate sales from assets###
+def get_asset_turnover_ratio(tick, period = 1):
+    avg_assets = get_avg_assets(tick, period = period)
+    net_revenue = get_net_revenue(tick, period = period)
+    return net_revenue / avg_assets
+
+### how many times a company’s inventory is sold and replaced ###
+def get_inventory_turnover_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    avg_inventory = get_avg_inventory(tick, period = period + 1)
+    return ist["CostOfRevenue"] / avg_inventory
+
+### average number of days that a company holds on to inventory###
+def get_day_sales_in_inventory_ratio(tick, period = 0):
+    avg_inventory = get_avg_inventory(tick, period = period)
+    return 365 / avg_inventory
+
+
+###
+### PROFITABILITY RATIOS: ability to generate income
+###
+
+### how much profit a company makes after paying its cost of goods sold###
+# getting 1.0, this can't be right
+def get_gross_margin_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    net_sales = get_net_sales(tick, period = period)
+    return ist["GrossProfit"] / net_sales
+
+### operating efficiency###
+def get_operating_margin_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period=period)
+    net_sales = get_net_sales(tick, period=period)
+    return ist["OperatingRevenue"] / net_sales
+
+### how efficiently a company is using its assets to generate profi###
+def get_return_on_assets_ratio(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    bs = fu.get_bs(tick, period = period)
+    return ist["NetIncome"] / bs["TotalAssets"]
+    
+### how efficiently a company is using its equity###
+def get_return_on_equity(tick, period = 0):
+    ist = fu.get_is(tick, period = period)
+    bs = fu.get_bs(tick, period = period)
+    return ist["NetIncome"] / bs["StockholdersEquity"]
+
+
+###
+### MARKET VALUE RATIOS: evaluate the share price of a company’s stock
+###
+
+### per-share value of a company based on the equity available to shareholders###
+def get_book_value_per_share_ratio(tick):
+    return  tick.info['bookValue']
+
+### amount of dividends attributed to shareholders###
+def get_dividend_yield_ratio(tick):
+    return tick.info["dividendYield"] * 100 * 4 / tick.info["currentPrice"] 
+
+### amount of net income earned for each share outstanding###
+def get_earnings_per_share(tick, period = 0):
+    ist = fu.get_is(tick, period=period)
+    return ist["DilutedEPS"]
+
+def get_income_per_share(tick):
+    return get_earnings_per_share(tick)
+
+### share price to its earnings per share###
+def get_price_to_earnings(tick):
+    earnings = get_earnings_per_share(tick)
+    return tick.info["currentPrice"] / earnings
+
+### how much investors are willing to pay per dollar of sales for a stock
+def get_price_to_sales(tick):
+    income_per_share = get_income_per_share(tick)
+    return tick.info["currentPrice"] / income_per_share
+
---
+++ rsi_util.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Zack West
+# This code was written by zack west and can be found here:
+# https://www.alpharithms.com/relative-strength-index-rsi-in-python-470209/
+#
+
+
+
+
+import utils.constants as const
+import pandas as pd
+
+
+def get_rsi(ticker, window_length=14, period="1mo", interval = "1d", price_field = 0):
+    """
+    get rsi from the data
+    lookback = number of days to use to calculate the rsi
+    price_field: 0=Close (Default), 1=Open, 2=High, 3=Low
+
+    returns data frame with price history including rsi
+
+    """
+    df = pd.DataFrame()
+
+    if price_field not in (0,1,2,3):
+        # print("field can only be a digit 0-3. 0=Close (default), 1=Open, 2=High, 3=Low")
+        return
+    
+    try:
+        data = ticker.history(period=period, interval=interval)
+    except Exception:
+        return None
+
+    if window_length > len(data):
+        # print(f"{ticker.info['symbol']} does not have enough data. window length {window_length} data items {len(data)}")
+        return None
+
+    df['diff'] = data[const.price_volume_fields[price_field]].diff(1)
+    df['gain'] = df['diff'].clip(lower=0).round(2)
+    df['loss'] = df['diff'].clip(upper=0).abs().round(2)
+
+    df['avg_gain'] = df['gain'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+    df['avg_loss'] = df['loss'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+
+    # Average Gains
+    for i, row in enumerate(df['avg_gain'].iloc[window_length+1:]):
+        df['avg_gain'].iloc[i + window_length + 1] =\
+        (df['avg_gain'].iloc[i + window_length] * (window_length - 1) + df['gain'].iloc[i + window_length + 1]) / window_length
+
+    # Average Losses
+    for i, row in enumerate(df['avg_loss'].iloc[window_length+1:]):
+        df['avg_loss'].iloc[i + window_length + 1] =\
+        (df['avg_loss'].iloc[i + window_length] * (window_length - 1) + df['loss'].iloc[i + window_length + 1]) / window_length
+
+    df['rs'] = df['avg_gain'] / df['avg_loss']
+    df['rsi'] = 100 - (100 / (1.0 + df['rs']))
+
+    data['rsi'] = df['rsi']
+    return data
+
