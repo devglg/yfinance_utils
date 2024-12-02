f77c12cc5ad29ca0e1b8aea2fe176f0d6aaaa2c3
L Gracia Tech <124262275+devglg@users.noreply.github.com>
2024-12-02 01:55:39 -0600

    just changing some stuff

---
+++ yfinance-utils/financials_utils.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Lehi Gracia
+#
+
+#
+
+"""
+GET FINANCIAL STATEMENTS as dictionary
+"""
+def get_is(ticker, period = 0):
+  return dict(ticker.get_income_stmt()[ticker.get_income_stmt().columns[period]])
+
+def get_bs(ticker, period = 0):
+  return dict(ticker.get_balance_sheet()[ticker.get_balance_sheet().columns[period]])
+
+def get_cashflow(ticker, period = 0):
+  return dict(ticker.get_cashflow()[ticker.get_cashflow().columns[period]])
+
+
+
+"""
+GET OTHER FINANCIAL INFORMATION
+"""
+
+def get_up_down_action(ticker):
+  d = ticker.get_upgrades_downgrades()
+  up = d[d.Action == "up"].shape[0]
+  down = d[d.Action == "down"].shape[0]
+  total = len(d)
+  return {"up":up, "down":down, "total":total}
+
+
+
+def get_historic_price(ticker, price_field = "Close"):
+  d = ticker.history()
+  return list(d[price_field])
+
+
+
+def get_last_price(tick):
+  d = get_historic_price(tick)
+  if len(d) < 1:
+    return None
+  return d[-1]
+
+
+
+
---
+++ yfinance-utils/rsi_util.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Zack West
+# This code was written by zack west and can be found here:
+# https://www.alpharithms.com/relative-strength-index-rsi-in-python-470209/
+#
+
+
+import pandas as pd
+
+
+def get_rsi(ticker, window_length=14, period="1mo", interval="1d"):
+    """
+    get rsi from the data
+    lookback = number of days to use to calculate the rsi
+    returns data frame with price history including rsi
+    """
+    df = pd.DataFrame()
+    try:
+        data = ticker.history(period=period, interval=interval)
+    except Exception:
+        return None
+
+    if window_length > len(data):
+        print(f"{ticker.info['symbol']} does not have enough data. window length {window_length} data items {len(data)}")
+        return None
+
+    df['diff'] = data["Close"].diff(1)
+    df['gain'] = df['diff'].clip(lower=0).round(2)
+    df['loss'] = df['diff'].clip(upper=0).abs().round(2)
+
+    df['avg_gain'] = df['gain'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+    df['avg_loss'] = df['loss'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+
+    # Average Gains
+    for i, row in enumerate(df['avg_gain'].iloc[window_length+1:]):
+        df['avg_gain'].iloc[i + window_length + 1] =\
+        (df['avg_gain'].iloc[i + window_length] * (window_length - 1) + df['gain'].iloc[i + window_length + 1]) / window_length
+
+    # Average Losses
+    for i, row in enumerate(df['avg_loss'].iloc[window_length+1:]):
+        df['avg_loss'].iloc[i + window_length + 1] =\
+        (df['avg_loss'].iloc[i + window_length] * (window_length - 1) + df['loss'].iloc[i + window_length + 1]) / window_length
+
+    df['rs'] = df['avg_gain'] / df['avg_loss']
+    df['rsi'] = 100 - (100 / (1.0 + df['rs']))
+
+    data['rsi'] = df['rsi']
+    return data
+
