e977a431655a9a3324aaa13850e675a159a32fae
L Gracia Tech <124262275+devglg@users.noreply.github.com>
2024-12-02 10:54:24 -0600

    rev up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yfinance_utils',
-    version='0.0.1',
+    version='0.0.2',
     description='a few methods to help with tech analysis and plotting',
     license='GPL 2.0',
     packages=find_packages(),
