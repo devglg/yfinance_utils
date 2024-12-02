0feb545d860cafeaaf8410d0249f664ca3855ac0
L Gracia Tech <124262275+devglg@users.noreply.github.com>
2024-12-02 06:49:57 -0600

    adding volume util

---
+++ yfinance_utils/volume_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2024 Lehi Gracia
+#
+
+import statistics
+
+def get_percent_volume(ticker, period='1mo', days_back=0):
+    """
+        period = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+        using more than 6mo may be useless
+
+        day = day to check against average. starts today and goes back
+
+        returns the percentage of volume / average for the period
+    """
+    periods_allowed = ['1d', '5d', '1mo', '3mo', '6mo']
+
+    if period not in periods_allowed:
+        print(f"period is invalid for this method. use {periods_allowed}")
+        return
+    
+    data = ticker.history(period = period)
+    volume = data["Volume"].iloc[len(data)-days_back]
+    average = statistics.mean(data["Volume"])
+    median = statistics.median(data["Volume"])
+    percentage = int(((volume/average)-1)*100)
+    return {"volume":volume, "percentage":percentage, "average":average, "median": median}
