5e0d2441eebcc2a9d76af910469bd890d4a9f488
L Gracia Tech <124262275+devglg@users.noreply.github.com>
2024-12-03 06:27:03 -0600

    changing method name to be clear

--- yfinance_utils/price_utils.py
+++ yfinance_utils/price_utils.py
@@ -4,7 +4,7 @@
 # Copyright 2024 Lehi Gracia
 #
 
-def get_number_intervals_rising(tick, period = "3mo", interval = "1wk"):
+def get_price_intervals_rising(tick, period = "3mo", interval = "1wk"):
     c = list(tick.history(period = period, interval = interval)["Close"])
     for i in range(len(c)-1, 2, -1):
         if c[i-1] < c[i]:
