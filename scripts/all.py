ea5317b9ffca2c4d95f82bf4fdc585a3528ecaea
Lehi Gracia <dev@glgracia.com>
2024-12-08 19:11:23 -0600

    adding to all

--- scripts/all.py
+++ scripts/all.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.call("c:/Users/mac1/Documents/GitHub/yfinance_utils/venv/Scripts/python.exe c:/Users/mac1/Documents/GitHub/yfinance_utils/test/daily_TTM_squeze.py")
 subprocess.call("c:/Users/mac1/Documents/GitHub/yfinance_utils/venv/Scripts/python.exe c:/Users/mac1/Documents/GitHub/yfinance_utils/test/daily_volume.py")
 subprocess.call("c:/Users/mac1/Documents/GitHub/yfinance_utils/venv/Scripts/python.exe c:/Users/mac1/Documents/GitHub/yfinance_utils/test/daily_upgrades.py")
+subprocess.call("c:/Users/mac1/Documents/GitHub/yfinance_utils/venv/Scripts/python.exe c:/Users/mac1/Documents/GitHub/yfinance_utils/test/daily_price_jumps.py")
