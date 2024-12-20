2819156d1e189492b1966c3d1e7bc710b93a7f31
Lehi Gracia <dev@glgracia.com>
2024-12-20 03:20:31 -0600

    clean 2

--- scripts/daily_TTM_squeze.py
+++ scripts/daily_TTM_squeze.py
@@ -19,26 +19,26 @@ start_time = timing_utils.start(filenames)
 
 for tick in filenames:
     try:
-        data = file_utils.read_historic_data(tick)        
+        data = file_utils.read_historic_data(tick)
+        if data['Close'].iloc[-1] < constants.MINIMUM_PRICE: continue
+
         vol_avg = data['Volume'].iloc[-1] / data['Volume'].mean() * 100
         bb = TA.BBANDS(data)
         kc = TA.KC(data, kc_mult=1.5)
         if bb['BB_UPPER'].iloc[-3] < kc['KC_UPPER'].iloc[-3] and \
-            bb['BB_UPPER'].iloc[-1]>kc['KC_UPPER'].iloc[-1] and \
-            data['Close'].iloc[-1] > bb['BB_UPPER'].iloc[-1] and \
-            data['Close'].iloc[-1] > constants.MINIMUM_PRICE:
-            print(tick,"<<<---------------------- HIGH ")     
+            bb['BB_UPPER'].iloc[-1] > kc['KC_UPPER'].iloc[-1] and \
+            data['Close'].iloc[-1] > bb['BB_UPPER'].iloc[-1]:
+            print(f"{tick:6}<<<----- HIGH ")     
 
             tmp = pd.DataFrame([[tick, data['Close'].iloc[-1], data['Volume'].iloc[-1], vol_avg, bb['BB_UPPER'].iloc[-3], kc['KC_UPPER'].iloc[-3], bb['BB_UPPER'].iloc[-1], kc['KC_UPPER'].iloc[-1] ]], columns=COLUMNS)  
             df_up = pd.concat([df_up,tmp], ignore_index=True)
 
         if bb['BB_LOWER'].iloc[-3] > kc['KC_LOWER'].iloc[-3] and \
             bb['BB_LOWER'].iloc[-1] < kc['KC_LOWER'].iloc[-1] and \
-            data['Close'].iloc[-1] < bb['BB_LOWER'].iloc[-1] and \
-            data['Close'].iloc[-1] > constants.MINIMUM_PRICE:
-            print(tick,"<<<---------------------- LOW ")     
+            data['Close'].iloc[-1] < bb['BB_LOWER'].iloc[-1]:
+            print(f"{tick:6}<<<----- LOW ")     
 
-            tmp = pd.DataFrame([[tick, data['Close'].iloc[-1], data['Close'].iloc[-1], vol_avg, bb['BB_LOWER'].iloc[-3], kc['KC_LOWER'].iloc[-3], bb['BB_LOWER'].iloc[-1], kc['KC_LOWER'].iloc[-1] ]], columns=COLUMNS)  
+            tmp = pd.DataFrame([[tick, data['Close'].iloc[-1], data['Volume'].iloc[-1], vol_avg, bb['BB_LOWER'].iloc[-3], kc['KC_LOWER'].iloc[-3], bb['BB_LOWER'].iloc[-1], kc['KC_LOWER'].iloc[-1] ]], columns=COLUMNS)  
             df_down = pd.concat([df_down,tmp], ignore_index=True)
         
     except Exception as e:
