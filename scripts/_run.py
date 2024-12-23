4eb545cd7d77cc77e71f99c194497d44d70440ac
Lehi Gracia <dev@glgracia.com>
2024-12-22 18:04:44 -0600

    something

--- scripts/_run.py
+++ scripts/_run.py
@@ -9,23 +9,20 @@ output = os.listdir(constants.OUTPUT_FOLDER)
 
 today = datetime.date.today()
 
-if today.strftime('%A') in ['Saturday', 'Sunday']:
-    print('')
-    print('***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***')
-    print('')
-else:
-    print('')
-    print('***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***')
-    print('')
-
 subprocess.call([f"./.venv/bin/python3", f"./scripts/snapshot.py"])
 
 for f in filenames:
     if today.strftime('%A') in ['Saturday', 'Sunday']:
+        print('')
+        print('***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***  WEEKLY  ***')
+        print('')
         if f.startswith('weekly'):
             print(f'Running script: {f}')
             subprocess.call([f"./.venv/bin/python3", f"./{constants.SCRIPTS_FOLDER_WEEKLY}/{f}"])
     else:
+        print('')
+        print('***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***  DAILY  ***')
+        print('')
         if f.startswith('daily'):
             print(f'Running script: {f}')
             subprocess.call([f"./.venv/bin/python3", f"./{constants.SCRIPTS_FOLDER_DAILY}/{f}"])
