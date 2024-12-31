46692042f1a96370176497ebfb243e74d1750b58
Lehi Gracia <dev@glgracia.com>
2024-12-31 16:57:24 -0600

    moving back to bigdata to make it easy to find info by TICK or DATE

--- yfinance_utils/file_utils.py
+++ yfinance_utils/file_utils.py
@@ -47,7 +47,7 @@ def get_datasets_list():
 def save_to_mongo(data, name):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['market']
-    collection = db[name]
+    collection = db['bigdata']
     recs = data.to_dict('records')
     
     def update_dicts_in_list(list_of_dicts, key, value):
