ef525a323c9659d5b962e878350857287284776a
Lehi Gracia <dev@glgracia.com>
2024-12-31 02:53:10 -0600

    fix dataframe index

--- scripts/daily/daily_volume_total.py
+++ scripts/daily/daily_volume_total.py
@@ -12,6 +12,7 @@ for tick in filenames:
         data = file_utils.read_historic_data(tick)
         if not 'DATE' in dfvol.columns:
             dfvol['DATE'] = data.index
+            dfvol.set_index('DATE', inplace=True)
 
         dfvol[tick] = data['Volume']
 
@@ -19,5 +20,6 @@ for tick in filenames:
         continue
 
 dfvol['TOTAL'] = dfvol.sum(axis=1)
+dfvol.reset_index(inplace=True)
 file_utils.save_output_file(dfvol, FILENAME)
 timing_utils.end(start_time, f'{FILENAME}')
