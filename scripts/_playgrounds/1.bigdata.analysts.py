8c828ecd05c7620d31e89e2004978a87e4def408
Lehi Gracia <dev@glgracia.com>
2025-01-03 15:11:49 -0600

    removing commented code

--- scripts/_playgrounds/1.bigdata.analysts.py
+++ scripts/_playgrounds/1.bigdata.analysts.py
@@ -21,7 +21,6 @@ res = get_all_data_from_script('daily_upgrades',
                                {}, 
                                {'_id':0, 'TOTAL':1, 'UP':1,'DOWN':1,'HOLD':1, 'TICK':1,'AVG':1})
 
-
 df = pd.DataFrame(res)
 df = df.sort_values(by='UP', ascending=False)
 
@@ -65,45 +64,5 @@ for i, row in df.iterrows():
                 width=2)
             ),row=1, col=1
     )
-
-
-# fig.append_trace(go.Scatter(x=df2['TICK'], y=df2['DOWN'], mode='markers', marker=dict(size=5, color='red'), name='DOWNGRADE DOWN'), row=2, col=1)
-# fig.append_trace(go.Scatter( x=df2['TICK'], y=df2['HOLD'], mode='markers', marker=dict(size=5, color='black'), name='DOWNGRADE HOLD'), row=2, col=1)
-# fig.append_trace(go.Scatter(x=df2['TICK'], y=df2['UP'], mode='markers', marker=dict(size=5, color='blue'), name='DOWNGRADE UP'), row=2, col=1)
-
-# for i, row in df2.iterrows():
-#     fig.add_shape(
-#         dict(type="line",
-#                 x0=row["TICK"],
-#                 x1=row["TICK"],
-#                 y0=0,
-#                 y1=row["DOWN"],
-#                 line=dict(
-#                 color="red",
-#                 width=2)
-#             ),row=2, col=1
-#     )
-#     fig.add_shape(
-#         dict(type="line",
-#                 x0=row["TICK"],
-#                 x1=row["TICK"],
-#                 y0=row["DOWN"],
-#                 y1=row["HOLD"],
-#                 line=dict(
-#                 color="black",
-#                 width=2)
-#             ),row=2, col=1
-#     )
-#     fig.add_shape(
-#         dict(type="line",
-#                 x0=row["TICK"],
-#                 x1=row["TICK"],
-#                 y0=row["HOLD"],
-#                 y1=row["UP"],
-#                 line=dict(
-#                 color="blue",
-#                 width=2)
-#             ),row=2, col=1
-#     )
 
 fig.show()
