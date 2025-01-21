1c341a5017d118e99823fc2e503063cb7305422b
Lehi Gracia <dev@glgracia.com>
2025-01-21 17:56:17 -0600

    updating tta squeeze shades

--- scripts/3_playgrounds/5.bigdata.TTASqueeze.py
+++ scripts/3_playgrounds/5.bigdata.TTASqueeze.py
@@ -43,21 +43,31 @@ for symbol in symbols:
     _d2 = pd.concat([_d2, TA.BBANDS(_d2)], axis=1)
     _d2.reset_index(inplace=True)
     fig.add_trace(
-        go.Scatter(y=_d2['Close'], x=_d2['Date'], name=symbol),
+        go.Scatter(y=_d2['Close'], x=_d2['Date'], name=symbol, showlegend=False, marker=dict(color="red",size=25)),
         secondary_y=False,
         row=counter, col=1
     )
     fig.add_trace(
-        go.Scatter(y=_d2['BB_UPPER'], x=_d2['Date'], name=f'BBU'),
+        go.Scatter(y=_d2['BB_UPPER'], x=_d2['Date'], name=f'BBU', showlegend=False, fill='tonexty', marker=dict(color='rgba(93, 138, 168, 0.9)',size=5)),
         secondary_y=False,
         row=counter, col=1
     )
     fig.add_trace(
-        go.Scatter(y=_d2['BB_LOWER'], x=_d2['Date'], name=f'BBL'),
+        go.Scatter(y=_d2['BB_LOWER'], x=_d2['Date'], name=f'BBL', showlegend=False, fill='tonexty', marker=dict(color='rgba(93, 138, 168, 0.9)',size=5)),
         secondary_y=False,
         row=counter, col=1
     )
+    price = f"{symbol} - Date: {today}, Close: {round(_d2['Close'].iloc[-1],2)}"
+    fig.add_annotation(xref='x domain',
+                    yref='y domain',
+                    x=0.01,
+                    y=1,
+                    text=price, 
+                    showarrow=False,
+                    font=dict(size=10),
+                    row=counter, col=1)
+
     counter = counter + 1
 fig.update_traces(mode='lines',line_shape='spline')
-fig.update_layout(height=4000)
+fig.update_layout(height=4000, title="TTA Squeeze")
 fig.show()
