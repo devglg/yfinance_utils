93cf1917b2dcdd9d17f6685a4882ab67f48cb3bd
Lehi Gracia @ GLGracia <124262275+devglg@users.noreply.github.com>
2025-03-15 22:37:16 -0500

    Update setup.py
    reving up to tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yfinance_utils',
-    version='0.0.5',
+    version='0.0.6',
     description='a few utilities to help with tech analysis and plotting',
     license='GPL 2.0',
     packages=find_packages(),
